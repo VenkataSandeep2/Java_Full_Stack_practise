c9022b0616155bce05774185773a552a2f6e1289
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-10 14:36:58 +0530

    Create Datatypes_2.java

---
+++ CORE JAVA/Basics/Datatypes_2.java
@@ -0,0 +1,26 @@
+class Datatypes_2
+  {
+    public static void main(String args[])
+      {
+      int i = 127;
+      float f =4.24f;
+      short s = 4500;
+      double d = 54.24;
+      long l = 10000000;
+      char ch ='c';
+      byte b =75;
+      boolean isJavaFun = true;
+
+      System.out.println("The Integer is :- " + i);
+      System.out.println("The Byte is :- " + b);
+      System.out.println("The Short is :- " + s);
+      System.out.println("The long is :- " + l);
+        System.out.println();
+      System.out.println("The float is :- " + f);
+      System.out.println("The double is :- " + d);
+        System.out.println();
+      System.out.println("The Char is :- " + ch);
+        System.out.println();
+      System.out.println("The Boolean is :- " + isJavaFun);
+      }
+  }
