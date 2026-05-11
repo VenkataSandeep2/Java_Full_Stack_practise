9f58852f3c72bca440a59f4d926fcc5c4854e606
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-11 16:09:11 +0530

    Update Operators.java

--- CORE JAVA/Basics/Operators.java
+++ CORE JAVA/Basics/Operators.java
@@ -10,13 +10,39 @@ class Operators
       System.out.println(a*b);
       System.out.println(a/b);
       System.out.println(a % b);      
-
-      System.out.println("Relational Operator");
+      System.out.println();
+        
+      System.out.println("assignment Operator");
       System.out.println(a += b);
       System.out.println(a -= b);
       System.out.println(a *= b);
       System.out.println(a /= b);
-      System.out.println(a %= b);      
+      System.out.println(a %= b);  
+      System.out.println();
+        
+      System.out.println(" Relational Operator");
+      System.out.println(a == b);
+      System.out.println(a != b);
+      System.out.println(a < b);
+      System.out.println(a > b);
+      System.out.println(a <= b);
+      System.out.println(a >= b);
 
+      System.out.println();
+      System.out.println("Logical Operator");
+      System.out.println(( a > b) && ( b < 10));
+      System.out.println(( a > b) || (b < 10));
+
+      System.out.println("Increment operator");
+      System.out.println(a++);
+      System.out.println(b++);
+      System.out.println(a--);
+      System.out.println(--b);
+      
+
+       System.out.println(" Ternary Operator ");
+        int min = (a < b) ? a : b;
+        System.out.println("Minimum is: " + min);
+      
     }
   }
