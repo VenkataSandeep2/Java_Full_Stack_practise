3685d0d1c3e6f5e12179e6e92689b27ae4ba4a5f
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-18 06:06:22 +0530

    Create IfCondition_Statement1.java

---
+++ CORE JAVA/ConditionalStatements_3/IfCondition_Statement1.java
@@ -0,0 +1,26 @@
+class IfCondition_Statement1
+  {
+    public static void main(String args[])
+    {
+      //10 > 5
+      //true
+      if(10 > 5)
+      //if(10<5)    doesnot print the output 
+      {
+        System.out.println("10 is big");
+      }
+      
+      if(10>4)
+      {
+          System.out.println("10 is big");
+        }
+        if(10 >= 10)
+        {
+            System.out.println("true");
+        }
+        if(20 != 20)
+        {
+            System.out.println(" true");
+        }
+    }
+  }
