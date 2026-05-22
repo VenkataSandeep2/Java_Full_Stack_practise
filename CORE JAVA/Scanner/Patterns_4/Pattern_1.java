9e7f7595e0ccb79f22fc23183313ef81d992771b
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-22 06:40:40 +0530

    Implement nested loop patterns in Pattern_1.java
    Added multiple patterns demonstrating nested loops in Java.

---
+++ CORE JAVA/Scanner/Patterns_4/Pattern_1.java
@@ -0,0 +1,119 @@
+class Patter_4
+   {
+     public static void main(String args[])
+    {
+    for(int i = 0; i<= 5 ;i++)
+    {
+        for(int j = 1;j<=5;j++)
+        {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+    }
+        
+    }
+  }
+
+/* 
+      OUTPUT
+
+   0 0 0 0 0
+ 1 1 1 1 1
+ 2 2 2 2 2
+ 3 3 3 3 3
+ 4 4 4 4 4
+ 5 5 5 5 5
+
+=== Code Execution Successful ===
+  */
+
+class Patter_4
+   {
+     public static void main(String args[])
+    {
+    for(int i = 0; i<= 5 ;i++)
+    {
+        for(int j = 1;j<=5;j++)
+        {
+            System.out.print(i);
+            System.out.println(j);
+        }
+        System.out.println();
+    }
+        
+    }
+  }
+  /*
+
+OUTPUT
+01
+02
+03
+04
+05
+
+11
+12
+13
+14
+15
+
+21
+22
+23
+24
+25
+
+31
+32
+33
+34
+35
+
+41
+42
+43
+44
+45
+
+51
+52
+53
+54
+55
+
+
+=== Code Execution Successful ===
+
+  */
+
+
+class Patter_4
+   {
+     public static void main(String args[])
+    {
+    for(int i = 0; i<= 5 ;i++)
+    {
+        for(int j = 1;j<=5;j++)
+        {
+            System.out.print(i+ "* " );
+        }
+        System.out.println();
+    }
+        
+    }
+  }
+
+/*
+  OUTPUT
+  0* 0* 0* 0* 0* 
+1* 1* 1* 1* 1* 
+2* 2* 2* 2* 2* 
+3* 3* 3* 3* 3* 
+4* 4* 4* 4* 4* 
+5* 5* 5* 5* 5* 
+
+=== Code Execution Successful ===
+
+  */
+  
