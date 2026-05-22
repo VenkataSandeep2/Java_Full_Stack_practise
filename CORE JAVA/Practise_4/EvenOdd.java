00f938d08adfa5470a01284599256ed60aee98c9
Venkatasandeepadapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-22 05:48:21 +0530

    Update EvenOdd.java

--- CORE JAVA/Practise_4/EvenOdd.java
+++ CORE JAVA/Practise_4/EvenOdd.java
@@ -18,3 +18,69 @@ class EvenOdd
     sc.close();
     }
 }
+
+/*
+
+OUTPUT
+
+Enter a number :- >  
+1
+1 is Odd Number
+
+=== Code Execution Successful ===
+
+Enter a number :- >  
+0
+0 Even number
+
+=== Code Execution Successful ===
+
+
+
+*/
+
+
+
+import java.util.*;
+class EvenOdd
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number :- >  ");
+        int num = sc.nextInt();
+
+        if(num == 0)
+        {
+            System.out.println("Nether even or odd");
+        }
+        else if( num % 2 ==0)
+        {
+            System.out.println( num +" Even number");
+        }
+        else
+            {
+                System.out.println(num +" is Odd Number");
+            }
+    sc.close();
+    }
+}
+
+/*
+
+OUTPUT
+Enter a number :- >  
+3154876
+3154876 Even number
+
+=== Code Execution Successful ===
+
+Enter a number :- >  
+0
+Nether even or odd
+
+=== Code Execution Successful ===
+
+
+
+*/
