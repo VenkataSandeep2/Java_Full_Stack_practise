1769c6c0d13273e0659687ef462111e9792c2e4d
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-05-27 21:38:17 +0530

    Update String1.java

--- CORE JAVA/Day-8__Strings/String1.java
+++ CORE JAVA/Day-8__Strings/String1.java
@@ -1,7 +1,12 @@
+
 class String1
-  {
-    public static void main(String args[])
-    {
-      String s1 = "Hiiii";
-    }
-  }
+{
+public static void main(String args[])
+	{
+	String s = new String("Venkata_Sandeep");
+	System.out.println(s);
+	System.out.println("The Object is:  "+s);
+	System.out.println("The Length of the String is :"+s.length());
+	System.out.println("Is itEmpty :"+s.isEmpty());
+	}
+}
