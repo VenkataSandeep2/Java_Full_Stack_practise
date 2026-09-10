a50617b7de610e82a1f397440656206f2751ea15
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-09-10 21:12:18 +0530

    Create Employee_1.java

---
+++ Spring_&_SpringBoot/_01_SingleTon/Exp_1/Employee_1.java
@@ -0,0 +1,26 @@
+package com.model;
+
+public class Employee_1 
+{
+	private int emp_id;
+	
+	public Employee_1()
+	{
+		
+	}
+
+	public int getEmp_id() {
+		return emp_id;
+	}
+
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [emp_id=" + emp_id + "]";
+	}
+	
+
+}
