4f6800e986d2b7e6251b2e8f11f5cd9ad086e80c
Venkata_Sandeep_adapala <216370168+VenkataSandeep2@users.noreply.github.com>
2026-09-10 21:13:54 +0530

    Create Main_2.java

---
+++ Spring_&_SpringBoot/_01_SingleTon/Exp_2/Main_2.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import com.model.Employee_2;
+
+public class Main_2
+{
+	public static void main(String[] args) 
+	{
+		
+		Employee_2 emp1 = getEmployee();
+		System.out.println(emp1.hashCode());
+		
+
+		Employee_2 emp2 = getEmployee();
+		System.out.println(emp2.hashCode());
+		
+
+		Employee_2 emp3 = getEmployee();
+		System.out.println(emp3.hashCode());
+	
+	}
+	
+	public static Employee_2 getEmployee()
+	{
+		Employee_2 emp = Employee_2.getEmployee_2Instance();
+		return emp;
+	}
+
+}
